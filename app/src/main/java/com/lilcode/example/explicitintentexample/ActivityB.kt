df581a5e88af6feaa85448d0a0ed32b2ee881009
NetLSS <futureprogrammer@naver.com>
2021-08-24 16:04:16 +0900

    Feat: 메인 엑티비티에서 B 엑티비티 실행 및 데이터 보내기

--- app/src/main/java/com/lilcode/example/explicitintentexample/ActivityB.kt
+++ app/src/main/java/com/lilcode/example/explicitintentexample/ActivityB.kt
@@ -2,10 +2,21 @@ package com.lilcode.example.explicitintentexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lilcode.example.explicitintentexample.databinding.ActivityBBinding
+import com.lilcode.example.explicitintentexample.databinding.ActivityMainBinding
 
 class ActivityB : AppCompatActivity() {
+    var _binding: ActivityBBinding? = null
+    val binding get() = requireNotNull(_binding)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_b)
+        _binding = ActivityBBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val extras = intent.extras ?: return
+
+        val qString = extras.getString("qString")
+        binding.textView2.text = qString
     }
 }
--- app/src/main/java/com/lilcode/example/explicitintentexample/MainActivity.kt
+++ app/src/main/java/com/lilcode/example/explicitintentexample/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.lilcode.example.explicitintentexample
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.lilcode.example.explicitintentexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    var _binding: ActivityMainBinding? = null
+    val binding get() = requireNotNull(_binding)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    fun sendText(view: View) {
+        val intent = Intent(this, ActivityB::class.java)
+        val myString = binding.editText1.text.toString()
+        intent.putExtra("qString", myString)
+        startActivity(intent)
     }
 }
